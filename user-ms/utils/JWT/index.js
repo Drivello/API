39fd2a19156c129ba8b43eaea8577d3cc3b2ffd2
Drivello <Nico_dd@outlook.com.ar>
2023-02-12 13:21:49 -0300

    added dotenv + jwt dependencies. added jwt signer util

---
+++ user-ms/utils/JWT/index.js
@@ -0,0 +1,8 @@
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const JWT = Object.freeze({
+    sign: (obj) => jwt.sign(obj, process.env.JWT_PK)    
+})
+export default JWT
